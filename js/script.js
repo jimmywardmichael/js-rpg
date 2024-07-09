07ea88e44ea7f6c9b623f31aa30c7301a4d92d18
jimmywardmichael <jimmywardmichael@gmail.com>
2024-07-08 22:44:00 -0500

    finished attack func

--- js/script.js
+++ js/script.js
@@ -183,7 +183,11 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -195,6 +199,10 @@ function attack() {
       defeatMonster();
     }
   }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex--;
+  }
 }
 
 function getMonsterAttackValue(level) {
@@ -203,6 +211,9 @@ function getMonsterAttackValue(level) {
   return hit > 0 ? hit : 0;
 }
 
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
+}
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
